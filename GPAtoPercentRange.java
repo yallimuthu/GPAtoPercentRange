89e443af8127e5b557dc267ef052399c66cef944
yallimuthu <63921803+yallimuthu@users.noreply.github.com>
2023-02-14 17:28:42 -0800

    Add files via upload

---
+++ GPAtoPercentRange.java
@@ -0,0 +1,24 @@
+public class GPAtoPercentRange {
+    public static void main(String[] args){ 
+        double GPA;
+        String grade;
+
+grade = "3.0";
+
+switch (grade){
+case "4.0":
+System.out.println("Marks >= 90");
+break;
+case "3.0":
+System.out.println("Marks [ >= 80 and <90 ]");
+break;
+case "2.0":
+System.out.println("Marks [ >= 70 and <80 ]");
+break;
+case "0.0":
+System.out.println("Marks [ >= 60]");
+break;
+default:
+}
+}
+}
