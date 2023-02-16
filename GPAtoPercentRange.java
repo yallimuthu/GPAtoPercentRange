f3d3f88661be4a7957f29222e82da471b7127d17
yallimuthu <63921803+yallimuthu@users.noreply.github.com>
2023-02-16 13:41:12 -0800

    Update GPAtoPercentRange.java

--- GPAtoPercentRange.java
+++ GPAtoPercentRange.java
@@ -3,22 +3,106 @@ public class GPAtoPercentRange {
         double GPA;
         String grade;
 
-grade = "3.0";
+grade = "2.0";
 
 switch (grade){
 case "4.0":
-System.out.println("Marks >= 90");
+System.out.println("grade >= 95");
+break;
+case "3.9":
+System.out.println("grade [ >=94  and <95 ]");
+break;
+case "3.8":
+System.out.println("grade [ >=93  and <94 ]");
+break;
+case "3.7":
+System.out.println("grade [ >=92  and <93 ]");
+break;
+case "3.6":
+System.out.println("grade [ >=91  and <92 ]");
+break;
+case "3.5":
+System.out.println("grade [ >=90  and <91 ]");
+break;
+case "3.4":
+System.out.println("grade [ >=89  and <90 ]");
+break;
+case "3.3":
+System.out.println("grade [ >=88  and <89 ]");
+break;
+case "3.2":
+System.out.println("grade [ >=87  and <88 ]");
+break;
+case "3.1":
+System.out.println("grade [ >=86  and <87 ]");
 break;
 case "3.0":
-System.out.println("Marks [ >= 80 and <90 ]");
+System.out.println("grade [ >=85  and <86 ]");
+break;
+case "2.9":
+System.out.println("grade [ >=84  and <85 ]");
+break;
+case "2.8":
+System.out.println("grade [ >=83  and <84 ]");
+break;
+case "2.7":
+System.out.println("grade [ >=82  and <83 ]");
+break;
+case "2.6":
+System.out.println("grade [ >=81  and <82 ]");
+break;
+case "2.5":
+System.out.println("grade [ >=80  and <81 ]");
+break;
+case "2.4":
+System.out.println("grade [ >=79 and <80 ]");
+break;
+case "2.3":
+System.out.println("grade [ >=78 and <79 ]");
+break;
+case "2.2":
+System.out.println("grade [ >=76 and <77 ]");
+break;
+case "2.1":
+System.out.println("grade [ >=75 and <76 ]");
 break;
 case "2.0":
-System.out.println("Marks [ >= 70 and <80 ]");
+System.out.println("grade [ >=74 and <75 ]");
+break;case "1.9":
+System.out.println("grade [ >=73 and <74 ]");
+break;
+case "1.8":
+System.out.println("grade [ >=72 and <73 ]");
+break;
+case "1.7":
+System.out.println("grade [ >=71 and <72 ]");
+break;
+case "1.6":
+System.out.println("grade [ >=70 and <71 ]");
+break;
+case "1.5":
+System.out.println("grade [ >=69 and <70 ]");
+break;
+case "1.4":
+System.out.println("grade [ >=68 and <69 ]");
+break;
+case "1.3":
+System.out.println("grade [ >=67 and <68 ]");
+break;
+case "1.2":
+System.out.println("grade [ >=66 and <67 ]");
+break;
+case "1.1":
+System.out.println("grade [ >=65 and <66 ]");
+break;
+case "1.0":
+System.out.println("grade [ >=64 and <65 ]");
 break;
 case "0.0":
-System.out.println("Marks [ >= 60]");
+System.out.println("grade [ >= 60 and <64]");
 break;
 default:
 }
 }
 }
+
